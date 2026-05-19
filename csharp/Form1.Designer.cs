be0389c2526ff5bf68961c665de5cec8b29c0d9b
lyrastudio <thiagu.mangara@gmail.com>
2026-05-19 15:17:00 -0300

    Gerado por AI Studio - 2026-05-19T18:16:59.845Z

---
+++ csharp/Form1.Designer.cs
@@ -0,0 +1,15 @@
+namespace CalculadoraApp {
+    partial class Form1 {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.BackColor = Color.FromArgb(51, 51, 51);
+        }
+    }
+}
