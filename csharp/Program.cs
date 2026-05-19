28a18d2cb6b8d546e6ec7962e726579463e50978
lyrastudio <thiagu.mangara@gmail.com>
2026-05-19 15:16:58 -0300

    Gerado por AI Studio - 2026-05-19T18:16:57.971Z

---
+++ csharp/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Windows.Forms;
+namespace CalculadoraApp {
+    static class Program {
+        [STAThread]
+        static void Main() {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
